a785735a508973b9c5cfba0fb22f21f651b4b1ee
vikash <vikash@DESKTOP-PNDM97R>
2017-08-18 11:40:40 +0530

    initial checkin

---
+++ student/src/main/java/student/dao/DB.java
@@ -0,0 +1,12 @@
+package student.dao;
+import java.sql.*;
+public class DB {
+public static Connection getCon(){
+	Connection con=null;
+	try{
+		Class.forName("oracle.jdbc.driver.OracleDriver");
+		con=DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe","system","password");
+	}catch(Exception ex){System.out.println(ex);}
+	return con;
+}
+}
